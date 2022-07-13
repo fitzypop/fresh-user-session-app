e0de8a81909c921e7149be6f95cbcd30a7c58e9c
fitzypop <fitzypop@pm.me>
2022-07-13 01:23:05 -0600

    add fauna example

--- routes/home.tsx
+++ routes/home.tsx
@@ -4,3 +4,4 @@ import { tw } from "@twind";
 import "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 
 const FAUNA_KEY = Deno.env.get("FAUNA_KEY") || "Nope";
+const FAUNA_DOMAIN = Deno.env.get("FAUNA_DOMAIN") || "db.fauna.com"
